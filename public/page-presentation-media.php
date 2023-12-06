df4d542711fb9b50faa7bafc8761a25a51852b33
Alek Miko <alekmikopro@gmail.com>
2023-12-06 17:41:41 +0100

    ajout de la page statique de présentation du produit + css

---
+++ public/page-presentation-media.php
@@ -0,0 +1,32 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>*Nom du média* / *Nom de l'auteur* - Book Your Media</title>
+    <link rel="stylesheet" href="./assets/css/page-presentation-media.css">
+</head>
+<body>
+    <section id="presentation-media">
+        <div class="left-side-page">
+            <p class="size-categorie">Catégorie : Livres</p>
+            <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSplyOJ8CsUZ9rOCySaoUY8SmYHiqqDj6UzvPR4aY9N14aw6AZj" alt="image de couverture" class="cover-picture">
+            <p class="size-available">Etat : <i>Disponible</i></p>
+            <button class="style-button">Réserver</button>
+        </div>
+        <div class="right-side-page">
+            <div class="info-labels-for-title box"><p>Titre</p></div>
+            <div class="info-labels-for-author box"><p>Auteur</p></div>
+            <div class="info-labels-for-date box"><p>Date de parution</p></div>
+            <div class="info-labels-for-description box"><p>Description</p></div>
+
+            <div class="data-of-media-title box"><p>Homo deus</p></div>
+            <div class="data-of-media-author box"><p>Yuval Noah Harari</p></div>
+            <div class="data-of-media-date box"><p>2015</p></div>
+            <div class="data-of-media-description box"><p>Homo Deus : Une brève histoire de l'avenir est un livre de Yuval Noah Harari publié pour la première fois en hébreu en 2015. Traduit en plusieurs langues, il est publié en traduction anglaise en 2016 et en française aux éditions Albin Michel en septembre 2017</p></div>
+        </div>
+    </section>
+</body>
+</html>
