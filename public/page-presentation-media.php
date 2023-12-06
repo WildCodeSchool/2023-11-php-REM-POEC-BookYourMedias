9e423d9f0e9c092c8349953e5d7fe5264be3c2d3
Alek Miko <alekmikopro@gmail.com>
2023-12-06 21:39:54 +0100

    correction of the margin

--- public/page-presentation-media.php
+++ public/page-presentation-media.php
@@ -5,15 +5,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>*Nom du média* / *Nom de l'auteur* - Book Your Media</title>
+    <title>Homo deus / Yuval Noah Harari - Book Your Media</title>
     <link rel="stylesheet" href="./assets/css/page-presentation-media.css">
 </head>
 <body>
+    <nav>
+        <img src="./assets/images/page-presentation-media/logo-book-your-medias.png" alt="logo du site" width="150px">
+    </nav>
     <section id="presentation-media">
         <div class="left-side-page">
-            <p class="size-categorie">Catégorie : Livres</p>
             <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSplyOJ8CsUZ9rOCySaoUY8SmYHiqqDj6UzvPR4aY9N14aw6AZj" alt="image de couverture" class="cover-picture">
             <p class="size-available">Etat : <i>Disponible</i></p>
+            <p class="size-categorie">Catégorie : Livres</p>
             <button class="style-button">Réserver</button>
         </div>
         <div class="right-side-page">
@@ -25,7 +28,7 @@
             <div class="data-of-media-title box"><p>Homo deus</p></div>
             <div class="data-of-media-author box"><p>Yuval Noah Harari</p></div>
             <div class="data-of-media-date box"><p>2015</p></div>
-            <div class="data-of-media-description box"><p>Homo Deus : Une brève histoire de l'avenir est un livre de Yuval Noah Harari publié pour la première fois en hébreu en 2015. Traduit en plusieurs langues, il est publié en traduction anglaise en 2016 et en française aux éditions Albin Michel en septembre 2017</p></div>
+            <div class="data-of-media-description box"><p>Homo Deus : Une brève histoire de l'avenir a été publié pour la première fois en hébreu en 2015. Traduit en plusieurs langues, il est publié en traduction anglaise en 2016 et en française aux éditions Albin Michel en septembre 2017</p></div>
         </div>
     </section>
 </body>
