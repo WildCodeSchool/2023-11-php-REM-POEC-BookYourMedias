5c4c1b9f2e24f684cffa25ac9a6d7c0db7114f1b
Alek Miko <alekmikopro@gmail.com>
2023-12-19 17:19:17 +0100

    add of a new SQL request during the command 'migration', which create a new table 'user'

--- migration.php
+++ migration.php
@@ -19,7 +19,11 @@ try {
     $pdo->exec('DROP DATABASE IF EXISTS ' . DB_NAME);
     $pdo->exec('CREATE DATABASE ' . DB_NAME);
     $pdo->exec('USE ' . DB_NAME);
-    $pdo->exec('CREATE TABLE user (id INT PRIMARY KEY AUTO_INCREMENT, adresse_email VARCHAR(255) NOT NULL, mot_de_passe VARCHAR(255), pseudo VARCHAR(45)  NOT NULL, role BOOLEAN NULL');
+    $pdo->exec('CREATE TABLE user (id INT PRIMARY KEY AUTO_INCREMENT, 
+    adresse_email VARCHAR(255) NOT NULL, 
+    mot_de_passe VARCHAR(255), 
+    pseudo VARCHAR(45)  NOT NULL, 
+    role BOOLEAN NULL');
     if (is_file(DB_DUMP_PATH) && is_readable(DB_DUMP_PATH)) {
         $sql = file_get_contents(DB_DUMP_PATH);
         $statement = $pdo->prepare($sql);
