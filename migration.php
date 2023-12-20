42058fc185a8ad48aff261fd8f0d36731730bf9d
Alek Miko <alekmikopro@gmail.com>
2023-12-20 12:30:13 +0100

    resolve error in migration.php

--- migration.php
+++ migration.php
@@ -27,12 +27,26 @@ try {
     $pdo->exec('CREATE TABLE emprunt (id INT PRIMARY KEY AUTO_INCREMENT,
     date_emprunt DATE,
     date_retour DATE,
-    FOREIGN KEY (id) REFERENCES user(id))');
-
-    $pdo->exec('CREATE TABLE auteur(id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR (150),
-    last_name VARCHAR(150),
-    FOREIGN KEY (id) REFERENCES medias(id))');
+    user_id INT,
+    medias_id INT,
+    FOREIGN KEY (id) REFERENCES user(id),
+    INDEX `fk_emprunt_user_idx` (`user_id` ASC) VISIBLE,
+    INDEX `fk_emprunt_medias1_idx` (`medias_id` ASC) VISIBLE,
+    CONSTRAINT `fk_emprunt_user`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `bookmediatest3v2`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    CONSTRAINT `fk_emprunt_medias1`
+    FOREIGN KEY (`medias_id`)
+    REFERENCES `bookmediatest3v2`.`medias` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)');
+    $pdo->exec('CREATE TABLE auteur (id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR (150) NULL,
+    last_name VARCHAR(150) NULL)');
+    $pdo->exec('CREATE TABLE categorie (id INT PRIMARY KEY AUTO_INCREMENT,
+    name_categorie VARCHAR(100))');
     $pdo->exec('CREATE TABLE medias (id INT PRIMARY KEY AUTO_INCREMENT,
     titre VARCHAR(150) NOT NULL,
     date DATE,
@@ -40,14 +54,16 @@ try {
     image_couverture VARCHAR(5000) NOT NULL,
     lien_extrait VARCHAR(5000)NOT NULL,
     disponible BOOLEAN NULL,
-    auteur_id INT NOT NULL,
-    FOREIGN KEY (id) REFERENCES categorie(id),
-    FOREIGN KEY (id) REFERENCES emprunt(id),
-    FOREIGN KEY (id) REFERENCES user(id))');
+    id_categorie INT,
+    id_user INT,
+    id_auteur INT,
+    id_emprunt INT,
+    FOREIGN KEY (id_categorie) REFERENCES categorie(id),
+    FOREIGN KEY (id_emprunt) REFERENCES emprunt(id),
+    FOREIGN KEY (id_user) REFERENCES user(id),
+    FOREIGN KEY (id_auteur) REFERENCES auteur(id))');
+    //ajouter des INSERT INTO pour les données à afficher
 
-    $pdo->exec('CREATE TABLE categorie (id INT PRIMARY KEY AUTO_INCREMENT,
-    name_categorie VARCHAR(100),
-    FOREIGN KEY (id) REFERENCES medias(id))');
 
     if (is_file(DB_DUMP_PATH) && is_readable(DB_DUMP_PATH)) {
         $sql = file_get_contents(DB_DUMP_PATH);
