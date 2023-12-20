b35892514cd0f6d2a52a2e259fc417870367414d
Alek Miko <alekmikopro@gmail.com>
2023-12-20 17:14:41 +0100

    add of the functionnality 'logout', the user can now logout  from his session

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Model\UserManager;
 use Twig\Environment;
 use Twig\Extension\DebugExtension;
 use Twig\Loader\FilesystemLoader;
@@ -12,6 +13,7 @@ use Twig\Loader\FilesystemLoader;
 abstract class AbstractController
 {
     protected Environment $twig;
+    protected array | false $user;
 
 
     public function __construct()
@@ -25,5 +27,13 @@ abstract class AbstractController
             ]
         );
         $this->twig->addExtension(new DebugExtension());
+        //Je récupère l'utilisateur dans la base de données si il existe
+        //Pour cela, j'utilise l'opérateur ternaire
+        $userManager = new UserManager();
+        /* Si la session existe, alors je vais chercher dans la base de données, sinon je retourne false */
+        $this->user = isset($_SESSION['user_id']) ? $userManager->selectOneById($_SESSION['user_id']) : false;
+        //Maintenant, tous les templates twig auront accès directement à user,
+        //sans devoir le préciser dans chaque controleur.
+        $this->twig->addGlobal('user', $this->user);
     }
 }
